0fb40c4635bcd79f215bb1abd0628485bea287d5
SemasPing <semasping@gmail.com>
2018-10-21 13:24:23 +0200

    fix fake 'title' and 'body' for news

--- database/factories/News.php
+++ database/factories/News.php
@@ -4,8 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\News::class, function (Faker $faker) {
     return [
-        'title' => $faker->title,
-        'body' => $faker->paragraph,
+        'title' => substr($faker->sentence(2), 0, -1),
+        'body' => $faker->realText(500),
         'category_id' => factory(\App\Category::class),
     ];
 });
